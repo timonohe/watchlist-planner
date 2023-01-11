2c932663cd74a8eb2a92dbe374658cfd8e756aee
Timo Nohe <timo.ademeit@gmx.de>
2023-01-11 17:18:36 +0100

    Initialer Commit

--- apps/watchlist-planner/src/app/app.module.ts
+++ apps/watchlist-planner/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {RouterOutlet} from "@angular/router";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterOutlet, BrowserAnimationsModule, MatToolbarModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
